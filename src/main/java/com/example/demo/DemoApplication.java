bc0dbe51fdfe30649b1ac93bb3c24e80fbcd3134
zhanghan1999 <826329367@qq.com>
2021-07-05 22:59:42 +0800

    'z1'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")  // mybatis 需要扫描mapper接口, 记得加上
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
